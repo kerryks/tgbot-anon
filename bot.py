7fedfdffda7bad4318a6f188ee2effe4d7e5de64
Karina Kolesnik <56219524+kerryks@users.noreply.github.com>
2025-03-08 13:08:35 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from aiogram.types import Message
 
 load_dotenv()
 # 🔑 Вставьте свой токен
-TOKEN = "7530075380:AAHW37SyfGCu6kPJnZHLgVqBLeUElYgSqSs"
+TOKEN = os.getenv("TOKEN")
 GROUP_ID = -4663207006  # ID вашей группы
 
 bot = Bot(token=TOKEN)
